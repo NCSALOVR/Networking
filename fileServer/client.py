2b08e2c0aec56e7c9058900f51ee97c62bc4a494
jbigg <jbigg.2012@gmail.com>
2015-03-11 16:06:02 -0500

    Added keep-alive and made a more robust client as well as a better protocol for the server. Note: Server now sends back an error code upon a bad a login, and sends back an ok upon successful login. Furthermore, client now has the command end to end a session with the server

--- fileServer/client.py
+++ fileServer/client.py
@@ -8,31 +8,44 @@ if __name__ == '__main__':
     host = '127.0.0.1'
     port = 8000
 
-    s = socket.socket()
-    s.connect((host, port))
-    mesg = sh.recv_msg(s)
-    action = raw_input(mesg)
-    id = 0
-    if action == "new":
-        sh.send_msg(s, action)
-        id = long(sh.recv_msg(s))
-        print id
-    else:
-        sh.send_msg(s, action)
+    while(True):
+        action = raw_input("Registration (new if new): ")
+        id = 0
+        if action == "end":
+            print "Goodbye"
+            quit()
+        s = socket.socket()
+        s.connect((host,port))
+        if action == "new":
+            sh.send_msg(s, action)
+            id = long(sh.recv_msg(s))
+        else:
+            sh.send_msg(s, action)
+        error_code = sh.recv_msg(s)
+        if not error_code == "ok":
+            print error_code
+            s.close()
+            continue
+        print "You are id "+str(id)
+        print "Use that id to login in the future"
 
-    #sending data to be used in update/delete to server
-    action = raw_input("Action(update/delete)? -> ")
-    sh.send_msg(s,action)
-    filePath = raw_input("Json file path to use for update/delete? -> ")
+        #sending data to be used in update/delete to server
+        while (True):
+            action = raw_input("Action(update/delete/end)? -> ")
+            sh.send_msg(s,action)
+            if action!="end":
+                 filePath = raw_input("Json file path to use for update/delete? -> ")
 
-    with open(filePath, 'rb') as f:
-        sh.send_msg(s, f.read())
-    f.close()
+                 with open(filePath, 'rb') as f:
+                     sh.send_msg(s, f.read())
+                 f.close()
 
-    #receiving the update from server
+             #receiving the update from server
 
-    serverData = sh.recv_msg(s)
-    print serverData
-    s.close()
+            serverData = sh.recv_msg(s)
+            print serverData
+            if serverData == "goodbye":
+                s.close()
+                break
 
     
--- fileServer/server.py
+++ fileServer/server.py
@@ -15,58 +15,75 @@ def threadFunc(conn):
     global central_json_data
     global profiles
     #check with registration
-    sh.send_msg(conn,"Registered?")
     id = sh.recv_msg(conn)
     p  = 0
     if id == "new":
         p = pro.Profile(id,central_json_data,profiles)
         profiles.append(p)
         sh.send_msg(conn, str(p.id))
-    else: 
+    else:
+        if not id.isdigit():
+            sh.send_msg(conn, "Error! "+id+" is NaN!")
+            print "Error! "+id+" is NaN!"
+            conn.close()
+            return
         t = long(id)
+        found = False
         for x in profiles:
             if x.id == t:
                 p = x
-        #TODO: when id is not a number and when id is not an id of the existing profile
+                found = True
+        if not found:
+            sh.send_msg(conn, "Error! Profile "+str(t)+" not found!")
+            print "Error! Profile "+str(t)+" not found! Closing connection..."
+            conn.close()
+            return
+    sh.send_msg(conn,"ok")
     id = p.id    
+    while(True):
+        #receiving data to be used in update/delete from client
+        action = sh.recv_msg(conn)
+        if action != 'end':
+            clientData = sh.recv_msg(conn)
+            local_json_data = json.loads(clientData)
+            print "File Received"
 
-    #receiving data to be used in update/delete from client
-    action = sh.recv_msg(conn)
-    clientData = sh.recv_msg(conn)
-    print "File Received"
+        #manipulate the central json based on the action and data from client    
+        if action == 'update':
+            if id in central_json_data:
+                central_json_data[id] = jm.update(central_json_data[id], local_json_data)
+            else:
+                central_json_data[id] = jm.update({},local_json_data)
+            for x in profiles:
+                if id in x.update:
+                    x.update[id] = jm.update(x.update[id],local_json_data)
+                else:
+                    x.update[id] = jm.update({},local_json_data)
 
-    #manipulate the central json based on the action and data from client    
-    local_json_data = json.loads(clientData)
-    if action == 'update':
-        if id in central_json_data:
-            central_json_data[id] = jm.update(central_json_data[id], local_json_data)
+        elif action == 'delete':
+            if id in central_json_data:
+                central_json_data[id] = jm.delete(central_json_data[id], local_json_data)
+            for x in profiles:
+                if id in x.delete:
+                    x.delete[id] = jm.update(x.delete[id],local_json_data)
+                else:
+                    x.delete[id] = jm.update({},local_json_data)
+        elif action == 'end':
+            sh.send_msg(conn, "goodbye")
+            print "Connection with profile "+str(id)+" closed"
+            conn.close() 
+            return 
+
+        #send the updated one back to client
+        if action == 'update':
+            sh.send_msg(conn, json.dumps(p.update))
+            p.update = {}
+        elif action == 'delete':
+            sh.send_msg(conn, json.dumps(p.delete))
+            p.delete = {}
         else:
-            central_json_data[id] = jm.update({},local_json_data)
-        for x in profiles:
-            if id in x.update:
-                x.update[id] = jm.update(x.update[id],local_json_data)
-            else:
-                x.update[id] = jm.update({},local_json_data)
-
-    elif action == 'delete':
-        if id in central_json_data:
-            central_json_data[id] = jm.delete(central_json_data[id], local_json_data)
-        for x in profiles:
-            if id in x.delete:
-                x.delete[id] = jm.update(x.delete[id],local_json_data)
-            else:
-                x.delete[id] = jm.update({},local_json_data)
-
-    #send the updated one back to client
-    if action == 'update':
-        sh.send_msg(conn, json.dumps(p.update))
-        p.update = {}
-    elif action == 'delete':
-        sh.send_msg(conn, json.dumps(p.delete))
-        p.delete = {}
-    else:
-        sh.send_msg(conn, json.dumps(central_json_data))
-    print central_json_data  
+            sh.send_msg(conn, json.dumps(central_json_data))
+        print central_json_data  
     conn.close()
 
 
